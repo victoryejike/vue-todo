915fec176a5aa3a908627c22bf15a6acb8767b34
Ejike Victory <victoryejike@gmail.com>
2021-06-26 23:48:17 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,137 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+<div class="container">
+  <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask"/>
+  <div v-show='showAddTask'>
+    <AddTask @add-task="addTask" />
+  </div>
+  
+  <Tasks @toggle-reminder='toggleReminder' @delete-task='deleteTask' :tasks="tasks"/>
+</div>
+ 
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Header from './components/Header.vue';
+import Tasks from './components/Tasks.vue';
+import AddTask from './components/AddTask.vue'
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header,
+    Tasks,
+    AddTask
+  }, 
+  data(){
+    return {
+      tasks: [],
+      showAddTask: false
+    }
   },
+  methods:{
+    async deleteTask(id){
+      if(confirm('Are you sure?')){
+        const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+        method:'DELETE',
+        })
+
+        res.status == 200 ? (this.tasks = this.tasks.filter(task=>task.id !== id)) : alert('Error deleting task')
+        
+      }
+    },
+    async toggleReminder(id){
+      const taskToToggle =  await this.fetchTask(id);
+      const updatedTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+      const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': "application/json"
+        },
+
+        body: JSON.stringify(updatedTask)
+      });
+
+      const data = await res.json()
+
+      this.tasks = this.tasks.map(task=>task.id === id ? {...task, reminder: data.reminder} : task)
+    },
+    async addTask(task){
+      const res = await fetch('http://localhost:5000/tasks', {
+        method:'POST',
+        headers: {
+          'Content-type': "application/json"
+        },
+
+        body: JSON.stringify(task)
+      });
+
+      const data = await res.json();
+
+      this.tasks = [...this.tasks, data]
+    },
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
+    async fetchData(){
+      const res = await fetch('http://localhost:5000/tasks');
+      const data = await res.json()
+
+      return data;
+    }, 
+    async fetchTask(id){
+      const res = await fetch(`http://localhost:5000/tasks/${id}`);
+      const data = await res.json()
+
+      return data;
+    }
+  },
+  async created(){ //lifecycle method in vue
+    this.tasks = await this.fetchData()
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: "Poppins", sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<template>
+    <button @click="$emit('toggle-add-task',onClick)" :style="{backgroundColor: color}" class='btn'>{{text}}</button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: ['text', 'color'],
+    }
+</script>
